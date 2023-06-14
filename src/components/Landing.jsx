7137c3d38b74acab16257af16f00b7166fbcc151
nadiauj <nadiauj@ext.inditex.com>
2023-06-14 18:23:44 +0200

    agregado de iframe para video de youtube

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,6 +4,7 @@ export const Landing = () => {
     <section className='landing-page'>
       <h1 className='title'>Somos MERMELADA.TECH</h1>
       <h2 className='sub-title'>Creando comunidad de mujeres en tecnología</h2>
+      <iframe src="https://www.youtube.com/embed/eBJNT8KV0ps" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </section>
   )
 }
