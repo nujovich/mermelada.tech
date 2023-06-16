566581f406be67852ea2175b117c8dae6ede8425
nadiauj <nadiauj@ext.inditex.com>
2023-06-16 17:22:30 +0200

    sumate a la comu button y aregado de efecto en landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,14 @@
 import './styles/landing.css'
 export const Landing = () => {
+  const onSumateOnClick = () => {
+    window.open("https://linktr.ee/mermeladatech", "_blank")
+  }
   return (
     <section className='landing-page'>
-      <h1 className='title'>Somos MERMELADA.TECH</h1>
-      <h2 className='sub-title'>Creando comunidad de mujeres en tecnología</h2>
+      <h1 className='title'>¡Hola! Somos MERMELADA.TECH</h1>
+      <h2 className='sub-title'>Una comunidad de mujeres en tecnología</h2>
       <iframe src="https://www.youtube.com/embed/eBJNT8KV0ps" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <button type='button' className='btn-sumate' onClick={() => onSumateOnClick()}>Sumate a la comu</button>
     </section>
   )
 }
